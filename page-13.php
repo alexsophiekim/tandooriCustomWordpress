ab635d02e90e548773213b388b95b420b2be1ba2
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-10-27 12:31:15 +1300

    about.php done

--- page-13.php
+++ page-13.php
@@ -1,20 +1,16 @@
 <?php get_header(); ?>
 
-<div class="container">
   <div class="row">
-    <div class="col col-2">
-        <h1 class="h4 welcome90">ABOUT</h1>
+    <div class="col col-2 bg-warning text-center">
+        <h1 class="h4 my-2">ABOUT</h1>
     </div>
-    <div class="col col-10">
+    <div class="col col-10 px-5 aboutBg">
       <?php if(have_posts()): ?>
       <?php while (have_posts()): the_post();  ?>
           <p><?php the_content(); ?></p>
-          <hr>
         <?php endwhile; ?>
       <?php endif; ?>
     </div>
   </div>
-
-</div>
 
 <?php get_footer(); ?>
