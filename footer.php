2215831b919cdc112908e3c5345b724ea54af3d3
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-10-26 14:51:37 +1300

    doing footer nav

--- footer.php
+++ footer.php
@@ -1,6 +1,13 @@
+  <?php if(has_nav_menu('footer_navigation')): ?>
 <footer>
-  <p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?></p>
+    <nav class="navbar mb-0 bg-dark tex-white">
+        <?php
+        wp_nav_menu( array( 'theme_location' => 'footer_navigation' ) );
+        ?>
+    </nav>
 </footer>
+<?php endif; ?>
+  <p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?></p>
 <?php wp_footer(); ?>
 </body>
 </html>
