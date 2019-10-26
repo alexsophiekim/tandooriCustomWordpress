f9b5b8ab2ed339f1c23d668a632fd4a1f3d582c7
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-10-26 14:43:03 +1300

    top_nav working now:

--- header.php
+++ header.php
@@ -7,30 +7,30 @@
     <?php wp_head(); ?>
   </head>
   <body>
-    <h1>header</h1>
-    <?php if(has_nav_menu('top-nav')): ?>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="<?php bloginfo('url'); ?>">
-      <?php if ( function_exists( 'the_custom_logo' ) ): ?>
-          <?php  the_custom_logo(); ?>
-        <?php else: ?>
-          <?php echo get_bloginfo('name'); ?>
-      <?php endif; ?>
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+    <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+      <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <?php wp_nav_menu(array(
-          'theme_loation'     => 'top_nav',
-          'depth'             => 2,
-          'container'         => 'div',
-          'container_class'   => 'collapse navbar-collapse',
-          'menu_class'        => 'nav navbar-nav',
-          'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-          'walker'            => new WP_Bootstrap_Navwalker(),
-        )); ?>
-      </div>
+        <a class="navbar-brand flex" href="<?php bloginfo('url'); ?>">
+          <?php if ( function_exists( 'the_custom_logo' ) ): ?>
+          <?php  the_custom_logo(); ?>
+            <?php else: ?>
+              <?php echo get_bloginfo('name'); ?>
+          <?php endif; ?>
+        </a>
+          <?php
+          wp_nav_menu( array(
+              'theme_location'    => 'top_nav',
+              'depth'             => 2,
+              'container'         => 'div',
+              'container_class'   => 'collapse navbar-collapse',
+              'container_id'      => 'bs-example-navbar-collapse-1',
+              'menu_class'        => 'nav navbar-nav',
+              'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+              'walker'            => new WP_Bootstrap_Navwalker(),
+          ) );
+          ?>
+        </div>
     </nav>
-  <?php endif; ?>
