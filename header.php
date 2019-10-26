2f68b08e79af55a36d5c5cba5bbe82baad903935
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-10-27 12:16:56 +1300

    headerf nav flex fixed

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
               'theme_location'    => 'top_nav',
               'depth'             => 2,
               'container'         => 'div',
-              'container_class'   => 'collapse navbar-collapse',
+              'container_class'   => 'd-flex justify-content-end collapse navbar-collapse',
               'container_id'      => 'bs-example-navbar-collapse-1',
               'menu_class'        => 'nav navbar-nav',
               'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
